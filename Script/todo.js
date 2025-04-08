f77177b4b34dce04b44a242005c53b4588fa1349
Kishore <kaliyappankishore9894@gmail.com>
2025-04-08 21:51:14 +0530

    to_do_change

--- Script/todo.js
+++ Script/todo.js
@@ -1,1 +1,19 @@
-
+let text;
+let date;
+let make_obj = {};
+let final_arr=[];
+function get_todo_list()
+{
+    text = document.getElementById('get_task_name').value;
+    date = document.getElementById('get_task_completion_date').value;
+    make_obj = {text,date};
+    final_arr.push(make_obj);
+    let get_html = '';
+    for(let i=0;i < final_arr.length;i++)
+    {
+        let obj = {};
+        obj = final_arr[i];
+        // {text,date} = {obj};
+        console.log(obj);
+    }
+}
