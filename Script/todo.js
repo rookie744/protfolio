749a47686acf62c51f1b0c8621889335621bc5a9
Kishore <kaliyappankishore9894@gmail.com>
2025-04-09 20:59:51 +0530

    to_do_9th_apr

--- Script/todo.js
+++ Script/todo.js
@@ -1,19 +1,38 @@
 let text;
 let date;
 let make_obj = {};
-let final_arr=[];
+let final_arr= JSON.parse(localStorage.getItem('to_do_list')) || [];
+let obj={};
+let get_html = '';
+final_arr.forEach((value,index) => {
+    obj = value;
+    get_html += `<p>${obj.text}</p><p>${obj.date}</p><p><button onclick="final_arr.splice(${index},1);Delete()">Delete</button></p>`;
+});
+document.querySelector('.to-do-list').innerHTML = get_html;
+get_html = '';
 function get_todo_list()
 {
     text = document.getElementById('get_task_name').value;
     date = document.getElementById('get_task_completion_date').value;
     make_obj = {text,date};
     final_arr.push(make_obj);
-    let get_html = '';
-    for(let i=0;i < final_arr.length;i++)
-    {
-        let obj = {};
-        obj = final_arr[i];
-        // {text,date} = {obj};
-        console.log(obj);
-    }
+    final_arr.forEach((value,index) => {
+        obj = value;
+        get_html += `<p>${obj.text}</p><p>${obj.date}</p><p><button onclick="final_arr.splice(${index},1);Delete()">Delete</button></p>`;
+    });
+    document.querySelector('.to-do-list').innerHTML = get_html;
+    console.log(get_html);
+    get_html = '';
+    localStorage.setItem('to_do_list',JSON.stringify(final_arr));
+}
+function Delete()
+{
+    final_arr.forEach((value,index) => {
+        obj = value;
+        get_html += `<p>${obj.text}</p><p>${obj.date}</p><p><button onclick="final_arr.splice(${index},1);Delete()">Delete</button></p>`;
+    });
+    document.querySelector('.to-do-list').innerHTML = get_html;
+    console.log(get_html);
+    get_html = '';
+    localStorage.setItem('to_do_list',JSON.stringify(final_arr));
 }
